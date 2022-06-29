74eec6dd55eba23c817ae93b0e6c12cab4f4d4f1
Lexutka <lexutka@gmail.com>
2022-06-29 19:19:25 +0700

    initial commit

--- test.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("postgresql+psycopg2://igkyezeo:nrzV9tYWcAMR2mHBPEfGWBP5li0osjsL@abul.db.elephantsql.com/igkyezeo")
-engine.connect()
-print(engine)
